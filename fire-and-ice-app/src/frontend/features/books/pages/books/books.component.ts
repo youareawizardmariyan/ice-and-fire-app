25e394d2caac3bafbec6bd475bc3071ebef3fc84
Mariyan <scorpionmillion1@gmail.com>
2026-04-20 13:19:12 +0300

    fixed todo + styling

--- fire-and-ice-app/src/frontend/features/books/pages/books/books.component.ts
+++ fire-and-ice-app/src/frontend/features/books/pages/books/books.component.ts
@@ -3,20 +3,22 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { BooksBEResponse } from '../../store';
 import { SearchService } from '../../services/search.service';
 import { BooksService } from '../../services/books.service';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-books',
   templateUrl: './books.html',
   imports: [MatCardModule, MatButtonModule, MatIconModule, MatProgressSpinnerModule],
 })
-export class BooksComponent implements OnInit {
+export class BooksComponent {
   private searchService = inject(SearchService);
   private booksService = inject(BooksService);
-  books = signal<BooksBEResponse>([]);
-  loading = signal(true);
+
+  books = toSignal(this.booksService.getBooks(), { initialValue: [] });
+
+  loading = computed(() => this.books().length === 0);
 
   filteredBooks = computed(() => {
     const books = this.books();
@@ -31,12 +33,4 @@ export class BooksComponent implements OnInit {
         book.country.toLowerCase().includes(searchTerm),
     );
   });
-
-  ngOnInit(): void {
-    // TODO avoid manual subscription
-    this.booksService.getBooks().subscribe((data) => {
-      this.books.set(data);
-      this.loading.set(false);
-    });
-  }
 }
